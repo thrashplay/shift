6adc8459972d2347115156610fbab04fbe852589
Sean Kleinjung <sean@thrashplay.com>
2022-07-13 16:04:33 -0500

    Add React lint rules.

--- src/preload.ts
+++ src/preload.ts
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
   for (const dependency of ['chrome', 'node', 'electron']) {
+    // eslint-disable-next-line no-console
     console.log(`${dependency} version: ${process.versions[dependency] ?? 'unknown'}`)
   }
 })
