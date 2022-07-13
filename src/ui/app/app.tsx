50e0a6f474fd9436372f448067a016cadf5d352f
Sean Kleinjung <sean@thrashplay.com>
2022-07-13 16:00:00 -0500

    Fixed resize issue with Pixi canvas.

--- src/ui/app/app.tsx
+++ src/ui/app/app.tsx
@@ -4,6 +4,11 @@ import { GameCanvas } from '../game-canvas'
 // to structure lower-level app components, provide state, etc.
 export const App = () => (
   <>
+    {/*
+      temporary title does not belong here, but was inserted just
+      to test non-Pixi components in same layout as canvas
+    */}
+    <h1>Game App</h1>
     <GameCanvas />
   </>
 )
